5aa6ae99c112a78fd781848a5361e35f3e0fe737
NikhilPatil333 <nikhilpatil30397@gmail.com>
2023-08-31 16:30:40 +0530

    SECOND GLUE PUSH

--- main.tf
+++ main.tf
@@ -1,41 +1,41 @@
 #Creating bucket
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "terraform-nikhil-prac"
-
-  tags = {
-    Name = "My bucket"
-  }
-}
-
-
-# for uploading a pyspark file into a bucket
-
-resource "aws_s3_object" "upload-glue-script" {
-  bucket = aws_s3_bucket.bucket.id
-  key    = "test.py"
-  source = "./test.py"
-}
-
-# 
-# resource "aws_glue_job" "glue_job" {
-#   name = "NYC-dataset-processing"
-#   role_arn = "arn:aws:iam::684710758112:role/LabRole"
-#   description = "This is script to convert dataset"
-#   max_retries = "1"
-#   timeout = 2880
+# resource "aws_s3_bucket" "bucket" {
+#   bucket = "terraform-nikhil-prac"
 
 #   tags = {
-#     Name = "MyGlueJob"
-#     AutoRun = "true"
+#     Name = "My bucket"
 #   }
-#   command {
-#     script_location = "s3://terraform-nikhil-prac/test.py"
-#     python_version = "3"
-#   }
-#   execution_property {
-#     max_concurrent_runs = 2
-#   }
-#   glue_version = "4.0"
-  
 # }
+
+
+# # for uploading a pyspark file into a bucket
+
+# resource "aws_s3_object" "upload-glue-script" {
+#   bucket = aws_s3_bucket.bucket.id
+#   key    = "test.py"
+#   source = "./test.py"
+# }
+
+# 
+resource "aws_glue_job" "glue_job" {
+  name = "NYC-dataset-processing"
+  role_arn = "arn:aws:iam::684710758112:role/LabRole"
+  description = "This is script to convert dataset"
+  max_retries = "1"
+  timeout = 2880
+
+  tags = {
+    Name = "MyGlueJob"
+    AutoRun = "true"
+  }
+  command {
+    script_location = "s3://terraform-nikhil-prac/test.py"
+    python_version = "3"
+  }
+  execution_property {
+    max_concurrent_runs = 2
+  }
+  glue_version = "4.0"
+  
+}
