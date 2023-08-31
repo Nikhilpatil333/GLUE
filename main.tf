7662c56edf97e984ceaa89c2c03b06f5fecf3b87
NikhilPatil333 <nikhilpatil30397@gmail.com>
2023-08-31 19:21:04 +0530

    GLUE PUSH

--- main.tf
+++ main.tf
@@ -19,11 +19,16 @@
 
 # 
 resource "aws_glue_job" "glue_job" {
-  name = "NYC-dataset-processing"
+  name = "group2"
   role_arn = "arn:aws:iam::684710758112:role/LabRole"
   description = "This is script to convert dataset"
   max_retries = "1"
   timeout = 2880
+
+  max_capacity             = 5    # Set the maximum number of worker nodes
+  number_of_workers        = 5    # Set the initial number of worker nodes
+  worker_type              = "G.1X"  # Specify the worker type
+
   command {
     script_location = "s3://terraform-nikhil-prac/test.py"
     python_version = "3"
@@ -32,12 +37,18 @@ resource "aws_glue_job" "glue_job" {
     max_concurrent_runs = 2
   }
   glue_version = "4.0"
-
-  tags = {
-    TriggerOnUpload = "true"
-  }
-  
+  StartOnCreation = "true"
 }
+
+# resource "aws_lambda_function" "glue_job_trigger_lambda" {
+#   filename         = "path/to/your/lambda_function.zip"
+#   function_name    = "GlueJobTriggerLambda"
+#   role             = "arn:aws:iam::684710758112:role/LabRole"  # Replace with your LabRole ARN
+#   handler          = "lambda_function.handler"
+#   runtime          = "python3.8"
+#   source_code_hash = filebase64sha256("path/to/your/lambda_function.zip")
+# }
+
 
 
 
