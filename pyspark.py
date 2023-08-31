a17fcfb56fd55f3dc638b5abbb457e375a005384
NikhilPatil333 <nikhilpatil30397@gmail.com>
2023-08-31 15:49:35 +0530

    FIRST GLUE PUSH

---
+++ pyspark.py
@@ -0,0 +1,19 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+# Initialize the GlueContext
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Source and destination S3 paths
+source_path = "s3://source-bucket/source-object"
+destination_path = "s3://destination-bucket/destination-object"
+
+job.commit()
